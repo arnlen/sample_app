09d1e89cd70f073b05c6155fc8ed5fbb8b7b8993
Arnlen <arlenglet@gmail.com>
2012-07-27 20:33:59 +0200

    Finish user edit, update, index and destoy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,26 @@
 FactoryGirl.define do
+
+  # Création d'un seul utilisateur
+=begin
   factory :user do
     name "Arnaud Lenglet"
     email "arnaud.lenglet@example.com"
     password "foobar"
     password_confirmation "foobar"
+  end
+=end
+
+#  Création d'une séquence d'utilisateurs
+  factory :user do
+    sequence(:name){|n| "Person #{n}"}
+    sequence(:email){|n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+  #  Admin factory
+    factory :admin do
+      admin true
+    end
+
   end
 end
