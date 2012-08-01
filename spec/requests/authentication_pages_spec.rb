9ae06252d68de0562b5f7def20b4e24296cd3988
Arnlen <arlenglet@gmail.com>
2012-08-01 22:50:53 +0200

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -99,18 +99,16 @@ describe "Authentication" do
           it {should have_selector('title', text: "Sign in")}
         end
 
-      end
-
-      describe "in the Microposts controller" do
-        describe "submitting to the create action" do
-          before {post microposts_path}
-          specify {response.should redirect_to(signin_path)}
+        describe "visiting the following page" do
+          before {visit following_user_path(user)}
+          it {should have_selector('title', text: 'Sign in')}
         end
 
-        describe "submitting to the destroy action" do
-          before {delete micropost_path(FactoryGirl.create(:micropost))}
-          specify {response.should redirect_to(signin_path)}
+        describe "visiting the followers page" do
+          before {visit followers_user_path(user)}
+          it {should have_selector('title', text: 'Sign in')}
         end
+
       end
 
       describe "when attempting to visit a protected page" do
@@ -131,6 +129,30 @@ describe "Authentication" do
               page.should have_selector('title', text: user.name)
             end
           end
+        end
+      end
+
+      describe "in the Microposts controller" do
+        describe "submitting to the create action" do
+          before {post microposts_path}
+          specify {response.should redirect_to(signin_path)}
+        end
+
+        describe "submitting to the destroy action" do
+          before {delete micropost_path(FactoryGirl.create(:micropost))}
+          specify {response.should redirect_to(signin_path)}
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before {post relationships_path}
+          specify {response.should redirect_to(signin_path)}
+        end
+
+        describe "submitting to the destroy action" do
+          before {delete relationship_path(1)}
+          specify {response.should redirect_to(signin_path)}
         end
       end
 
